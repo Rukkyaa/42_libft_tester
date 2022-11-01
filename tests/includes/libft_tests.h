f290f24ee9d1a88e61c0f213363c91e3255d9b30
Rukkyaa <lbrt.axel@gmail.com>
2022-11-01 15:51:20 +0100

    Remove my personal libft

--- tests/includes/libft_tests.h
+++ tests/includes/libft_tests.h
@@ -6,7 +6,7 @@
 /*   By: rukkyaa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 19:07:11 by rukkyaa           #+#    #+#             */
-/*   Updated: 2022/11/01 13:49:29 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2022/11/01 15:50:39 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define LIBFT_TESTS_H
 
 # include "../../framework/includes/libunit.h"
-# include "../libft/libft.h"
+# include "../../../libft/libft.h"
 
 # include <ctype.h>
 # include <string.h>
